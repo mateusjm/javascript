a4c9f3b4443ced7fe4e32ba64d47d9858a22a3bc
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2023-08-19 01:00:46 -0300

    atualizei o ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,25 @@
-alert('Olá!')
+function gerar() {
+
+    var num = document.getElementById('inum')
+    var tal = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        var n = Number(num.value)
+        var c =1
+        tal.innerHTML = ""
+       /*  while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }  */
+        for (c != n; c<=10; c++) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab ${c}`
+            tal.appendChild(item)
+        }
+}
+}
