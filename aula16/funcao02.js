97e112ccc9ae2cdde865557ed46928c8da82bfc0
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2023-08-23 20:24:01 -0300

    aula 016

---
+++ aula16/funcao02.js
@@ -0,0 +1,9 @@
+function soma(n1=0, n2=0){
+    // se n1 ou n2 não tiverem valor eles assumirão o valor de 0
+    return n1 + n2
+}
+
+console.log(soma( 2, 5))
+    // console.log mostra na tela 
+    // (soma) é o primiero parametro
+    // (n1, n2) é outro parametro 
